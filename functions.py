9b8cce3cf16da75a2bd391b9553413db58606546
parkss0612 <seongsub344@gmail.com>
2025-05-21 14:39:30 +0900

    functions.py

--- functions.py
+++ functions.py
@@ -1,20 +1,12 @@
 import cmath
 
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
 def minus(a, b):
     """Subtract b from a."""
     return a - b
-
-def multiply(a, b):
-    """Multiply two numbers."""
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        return '0으로 나눌 수 없음.'
-    return a / b
-
-def make0(a):
-    return a*0
 
 def root(e):
     """Return the square root of a. Supports complex results for negative inputs."""
